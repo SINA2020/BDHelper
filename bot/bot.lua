7738319823c29fa231b34169023da6fd8d07d258
SINA2020 <shomareh.majazi.nice@gmail.com>
2017-06-12 22:38:28 +0430

    Create bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -2,7 +2,7 @@ package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
 bot_token = "334612749:AAH5YRUK9cSnPii7FAsZmNrA3wTOUr8O-ys" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 376398390
+sudo_id = 372530129
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
@@ -162,7 +162,7 @@ function create_config( )
     "core",
     "plugins"
     },
-    sudo_users = {376398390,376398390},--Sudo users
+    sudo_users = {376398390,372530129},--Sudo users
     admins = {},
     disabled_channels = {},
     moderation = {data = ''..BeyondTeam..'/data/moderation.json'},
@@ -182,7 +182,7 @@ _》Developer & Sponser :_ [MM](Telegram.Me/MM20Nice)
 *》Our channel :*
 》[Nice20Team](Telegram.Me/Nice20Team)
 
-*》Our Site :*
+*》Our team :*
 》[Nice20Team]()
 ]],
   }
